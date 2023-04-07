0f8fa75970f107958331037f6c57bf3df3c9f471
Mohtasham <96950546+MohtashamASiddiqui@users.noreply.github.com>
2023-04-07 05:46:19 +0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import memories from "./images/memories.png";
+import makeStyles from "./styles";
+
+const App = () => {
+  const classes = makeStyles();
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <Container maxidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            Memories
+          </Typography>
+          <img
+            className={classes.image}
+            src={memories}
+            alt="memories"
+            height="60"
+          />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="stretch"
+              spacing={4}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={7}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+import makeStyles from "./styles";
+
+const Form = () => {
+  const classes = makeStyles();
+  return <h1>FORM</h1>;
+};
+
+export default Form;
